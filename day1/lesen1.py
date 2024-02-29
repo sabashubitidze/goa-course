99186f427f3f8ac13eaaadb5a3f4093dd2a9ede1
sabashubitidze <sabashubitidze210@gmail.com>
2024-02-29 23:48:35 +0400

    first commit

---
+++ day1/lesen1.py
@@ -0,0 +1,31 @@
+name ="saba"
+
+surname = "shubitidze"
+
+
+print(name)
+
+
+print(surname)
+
+age = "14"
+
+
+hieght = "155"
+
+is_ugly=False
+
+
+isUGLY=False
+
+print(name+""+surname)
+
+
+print(name+""+hieght)
+
+print(name+""+age)
+
+
+
+
+#მე მქვია საბა შუბითიდზე ვარ 13 წლის 10 მარტს ვხდები  14 წლის ვსწავლობ 145 სკოლაში ვარ 8 კლასში  მიყვარს ძილი და კომპიუტერი ასევე მიყვარს ბევრი რამის შესწავლა
