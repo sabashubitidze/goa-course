a0ddfb9851ea85ceb55e2ce13c211ef437581a93
sabashubitidze <sabashubitidze210@gmail.com>
2024-03-02 14:54:52 +0400

    first commit

---
+++ day4/home work/homework/leson.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+name = input("enter your name: ")
+print(name) 
+#ეს პროგრამა გვეკთხება თქვენს სახელს და გვარს
+age = input("enter your age: ")
+print("age")
+#ეს პროგრამა თქვენს ასაკს
+name("saba")
+#აქ უნდა ჩავწეროთ თქვენი სახელი რო ამოვიდეს დანაწერი name(saba)
+age("saba")
+#აქ უნდა ჩავწეროთ თქვენი ასაკი და დაიწერება age(14)
